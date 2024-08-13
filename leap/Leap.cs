ba08e89e146a4a15bdb6a6a0e25deab2e043ad86
matheus <matheusvictor870@gmail.com>
2024-08-13 19:09:12 -0300

    Optimize leap

--- leap/Leap.cs
+++ leap/Leap.cs
@@ -2,18 +2,5 @@ using System;
 
 public static class Leap
 {
-    public static bool IsLeapYear(int year)
-    {
-        if (year % 4 == 0)
-        {
-            if (year % 100 == 0)
-            {
-                if (year % 400 == 0)
-                    return true;
-                return false;
-            }
-            return true;
-        }
-        return false;
-    }
+    public static bool IsLeapYear(int year) => (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
 }
