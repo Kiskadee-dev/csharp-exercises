a2e8183d901f0405f7951af0e855342428b20447
matheus <matheusvictor870@gmail.com>
2024-08-12 22:01:18 -0300

    Solve Tim from Marketing

--- tim-from-marketing/TimFromMarketing.cs
+++ tim-from-marketing/TimFromMarketing.cs
@@ -2,8 +2,5 @@ using System;
 
 static class Badge
 {
-    public static string Print(int? id, string name, string? department)
-    {
-        throw new NotImplementedException("Please implement the (static) Badge.Print() method");
-    }
+    public static string Print(int? id, string name, string? department) => $"{(id == null ? String.Empty : $"[{id}] -")} {name} - {(department == null ? "OWNER" : department.ToUpper())}".Trim();
 }
